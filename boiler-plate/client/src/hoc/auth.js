a5e83aa3ebc9f9034d86f71c9752d6bbc605531c
canadanam <tjdwoocn@gmail.com>
2022-05-12 19:28:05 +0900

    8장 진행

--- boiler-plate/client/src/hoc/auth.js
+++ boiler-plate/client/src/hoc/auth.js
@@ -1,50 +1,89 @@
-import React, { useEffect } from 'react';
-// import Axios from 'axios';
-import { useDispatch } from 'react-redux';
-import { auth } from '../_actions/user_action';
-import { useNavigate } from 'react-router-dom'
+// import React, { useEffect } from 'react';
+// // import Axios from 'axios';
+// import { useDispatch } from 'react-redux';
+// import { auth } from '../_actions/user_action';
+// import { useNavigate } from 'react-router-dom'
 
 
-export default function (SpecificComponent, option, adminRoute = null) {
+// export default function (CompoesedClass, option, adminRoute = null) {
 
-    // 옵션의 종류
-    // null: 아무나 출입이 가능한 페이지
-    // true: 로그인한 유저만 출입이 가능한 페이지
-    // flase: 로그인한 유저는 출입 불가능한 페이지
-    function AuthenticationCheck(props) {
-        let navigate = useNavigate();
-        const dispatch = useDispatch();
+//     // 옵션의 종류
+//     // null: 아무나 출입이 가능한 페이지
+//     // true: 로그인한 유저만 출입이 가능한 페이지
+//     // flase: 로그인한 유저는 출입 불가능한 페이지
+//     function AuthenticationCheck(props) {
+//         let user = useSelector(state => state.user);
+//         let navigate = useNavigate();
+//         const dispatch = useDispatch();
         
-        useEffect( () => {
+//         useEffect( () => {
             
-            dispatch(auth()).then(response => {
-                console.log(response)
-                // 로그인 하지 않은 상태
-                if(!response.payload.isAuth) {
-                    if(option) {
-                        navigate("/login")
-                    }
-                } else {
-                // 로그인 한 상태
-                    if(adminRoute && !response.payload.isAdmin) {
-                        // admin이 아닌데 admin만 들어갈 수 있는 페이지에 들어가려하는 경우
-                        navigate("/")
+//             dispatch(auth()).then(response => {
+//                 console.log(response)
+//                 // 로그인 하지 않은 상태
+//                 if(!response.payload.isAuth) {
+//                     if(option) {
+//                         navigate("/login")
+//                     }
+//                 } else {
+//                 // 로그인 한 상태
+//                     if(adminRoute && !response.payload.isAdmin) {
+//                         // admin이 아닌데 admin만 들어갈 수 있는 페이지에 들어가려하는 경우
+//                         navigate("/")
 
+//                     } else {
+//                         if(option===false){
+//                             navigate("/")
+//                         }
+//                     }
+
+//                 }
+
+//             })
+
+//         }, [dispatch])
+//         return (
+//         <CompoesedClass {…props} user={user}/>
+//         )
+//     }
+
+//     return AuthenticationCheck
+// }
+
+
+import React, {useEffect} from 'react';
+import { authUser } from '../_actions/user_action';
+import {useDispatch, useSelector} from "react-redux";
+import {useNavigate} from "react-router-dom";
+
+export default function (CompoesedClass, reload, adminRoute = null) {
+
+    function AuthenticationCheck(props) {
+        let user = useSelector(state => state.user);
+        const dispatch = useDispatch();
+        const navigate = useNavigate();
+
+        useEffect(() => {
+            dispatch(authUser())
+                .then(async response => {
+                    if (await !response.payload.isAuth) {
+                        // 로그인하지 않은 상태
+                        if (reload) {
+                            navigate('/login')
+                        }
                     } else {
-                        if(option===false){
-                            navigate("/")
+                        // 로그인한 상태
+                        if (adminRoute && !response.payload.isAdmin) {
+                            navigate('/')
+                        } else {
+                            if (reload === false)
+                                navigate('/')
                         }
                     }
-
-                }
-
-            })
-
-        }, [])
-        return (
-        <SpecificComponent />
-        )
+                })
+        }, [dispatch])
+        return (<CompoesedClass {...props} user={user}/>)
     }
 
-    return AuthenticationCheck
+    return AuthenticationCheck;
 }
--- boiler-plate/server/middleware/auth.js
+++ boiler-plate/server/middleware/auth.js
@@ -4,10 +4,11 @@ let auth = (req, res, next) => {
     // 인증 처리
 
     // 1. 클라이언트 쿠키에서 토큰을 가져온다.
-    let token = req.cookies.x_auth;
+    let token = req.cookies.w_auth;
 
     // 2. 토큰을 복호화한 후 User를 찾는다.
     User.findByToken(token, (err, user) => {
+
         // User 가 없으면 인증 No
         if (err) throw err;
         if (!user) return res.json({isAuth: false, err: true})
